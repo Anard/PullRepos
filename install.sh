65f6fad17366a45d9a855b9f14872c7efcaaaf7f
Anard <anard@free.fr>
2020-01-28 11:14:31 +0100

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,43 +1,47 @@
 #!/bin/sh
-echo Installation des scripts...
-if ! [ -f /usr/local/lib/help.cnf ]; then
-	if [ -d HelpSh ]; then
-		del=0
-		cd HelpSh
-		git pull
-		ret=$?
-		cd ..
-	else
-		del=1
-		git clone https://github.com/Anard/HelpSh.git
-		ret=$?
-	fi
-	if [ $ret -gt 0 ]; then 
-		echo "Une erreur est survenue lors du clonage de HelpSh"
-		exit $ret
-	fi
-	sudo cp HelpSh/*.cnf /usr/local/lib/
+configgPath="/usr/local/lib"
+scriptPath="/usr/local/bin"
+
+echo "Récupération et installation du dépôt HelpSh"
+if [ -d HelpSh ]; then
+	del=0
+	cd HelpSh
+	git pull
+	ret=$?
+	cd ..
+else
+	del=1
+	git clone https://github.com/Anard/HelpSh.git
+	ret=$?
+fi
+if [ $ret -gt 0 ]; then 
+	echo "Une erreur est survenue lors du clonage de HelpSh"
+	exit $ret
+fi
+sudo cp HelpSh/*.cnf ${configPath}/
+ret=$?
+if [ $ret -gt 0 ]; then
+	echo "Une erreur est survenue lors de la copie des fichiers de configuration"
+	exit $ret
+fi
+
+echo "Installation de PullRepos"
+if ! [ -f ${configPath}/repos.cnf ]; then
+	sudo cp *.cnf ${configPath}/
+	sudo chmod +r ${configPath}/repos*.cnf ${configPath}/help.cnf 
 	ret=$?
 	if [ $ret -gt 0 ]; then
 		echo "Une erreur est survenue lors de la copie des fichiers de configuration"
 		exit $ret
 	fi
-else del=0
 fi
-if ! [ -f /usr/local/lib/repos.cnf ]; then
-	sudo cp *.cnf /usr/local/lib
-	sudo chmod +r /usr/local/lib/repos*.cnf /usr/local/lib/help.cnf 
-	ret=$?
-	if [ $ret -gt 0 ]; then
-		echo "Une erreur est survenue lors de la copie des fichiers de configuration"
-		exit $ret
-	fi
-fi
+
 if [ $del -gt 0 ]; then
 	sudo rm -r HelpSh
 fi
-sudo cp repos-pull /usr/local/bin
-sudo chmod +x /usr/local/bin/repos-pull
+sudo cp repos-pull ${scriptPath}/
+sudo chmod +x ${scriptPath}/repos-pull
 echo "Installation terminée"
+echo "Configurez ${configPath}/repos.cnf et ${configPath}/repos-scripts.cnf pour vos dépôts"
 
 exit 0
